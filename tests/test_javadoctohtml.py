5244e1eef3a7f64d18e5883b2df383f2b6fef9b3
Moultree <okaykudes@gmail.com>
2023-04-13 23:19:22 +0600

    added javadoctohtml tests

--- tests/test_javadoctohtml.py
+++ tests/test_javadoctohtml.py
@@ -1,6 +1,7 @@
 import unittest
 import os
 import sys
+import subprocess
 from pathlib import Path
 
 sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
@@ -11,8 +12,24 @@ class TestJavaDocToHtml(unittest.TestCase):
     def test_empty_file(self):
         Path("tests/temp").mkdir(parents=True, exist_ok=True)
 
-        with open("tests/temp/empty.java", "w") as f:
-            f.write("")
+        with open("tests/temp/empty.java", "w", encoding="utf-8") as file:
+            file.write("")
 
         JavaDocToHtml("tests/temp/empty.java")
         self.assertTrue(os.path.isfile("out/empty.html"))
+    
+    def test_directory(self):
+        Path("tests/temp").mkdir(parents=True, exist_ok=True)
+
+        with open("tests/temp/empty.java", "w", encoding="utf-8") as file:
+            file.write("")
+        
+        JavaDocToHtml("tests/temp")
+        self.assertTrue(os.path.isfile("out/empty.html"))
+
+    def test_wrong(self):
+        before = os.listdir("out")
+        JavaDocToHtml("tests/nothing.java")
+        after = os.listdir("out")
+
+        self.assertEqual(len(before), len(after))
