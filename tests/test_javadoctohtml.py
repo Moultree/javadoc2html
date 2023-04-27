1c3eabcdfddaea30957a64aae6292fe95b41432e
Moultree <okaykudes@gmail.com>
2023-04-27 22:27:49 +0600

    removed unnecessary import

--- tests/test_javadoctohtml.py
+++ tests/test_javadoctohtml.py
@@ -1,7 +1,6 @@
 import unittest
 import os
 import sys
-import subprocess
 from pathlib import Path
 
 sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
@@ -17,13 +16,13 @@ class TestJavaDocToHtml(unittest.TestCase):
 
         JavaDocToHtml("tests/temp/empty.java")
         self.assertTrue(os.path.isfile("out/empty.html"))
-    
+
     def test_directory(self):
         Path("tests/temp").mkdir(parents=True, exist_ok=True)
 
         with open("tests/temp/empty.java", "w", encoding="utf-8") as file:
             file.write("")
-        
+
         JavaDocToHtml("tests/temp")
         self.assertTrue(os.path.isfile("out/empty.html"))
 
